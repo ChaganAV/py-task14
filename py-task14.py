d34217b6fe961021055005b05c4e392a1044eb9f
Alexandr <jwchaganav@gmail.com>
2022-12-17 15:26:37 +0400

    обернул в try

--- py-task14.py
+++ py-task14.py
@@ -1,3 +1,15 @@
-import function as func
+# import function as func
+# func.PowerInt(30)
 
-func.PowerInt(30)
+def PowerInt(max,num = 2):
+    res = num
+    while res <= max:
+        print(res)
+        res = res * num
+
+try:
+    num = int(input("Введите число для возведения в степень: "))
+    max = int(input("Введите число N (предел степени):" ))
+    PowerInt(max,num)
+except:
+    print("По условиям задачи требуется число")
